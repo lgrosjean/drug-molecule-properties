6222712bc0c52adf5a3e8a324f22481c671c98f1
Léo GROSJEAN <leo.grosjean@ekimetrics.com>
2021-02-02 11:41:27 +0100

    new: add project requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+from model import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drug-molecule-properties",
+    version=__version__,
+    author="Léo Grosjean",
+    author_email="leo.grosjean@live.fr",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lgrosjean/drug-molecule-properties",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": ["servier=src.__main__:main"],
+    },
+)
